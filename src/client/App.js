5719d25cc4acfed023ba68308044fd212c86c851
shravyagrk@gmail.com <shravyagrk@gmail.com>
2022-10-31 16:01:04 +0100

    added header component

--- src/client/App.js
+++ src/client/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import TestComponent from "./components/TestComponent/TestComponent";
+import Header from "./components/Header";
 
 function App() {
   return (
     <Router>
       <Route exact path="/">
+        <Header></Header>
         <p>test</p>
       </Route>
       <Route exact path="/lol">
