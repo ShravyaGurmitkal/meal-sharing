997de191af63539d13dc7dd52f51d8739ca61a5f
shravyagrk@gmail.com <shravyagrk@gmail.com>
2022-11-06 03:58:57 +0100

    fetching all meals jsx file

--- src/client/components/Allmeals.jsx
+++ src/client/components/Allmeals.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { useLocation } from 'react-router-dom'
+import "./MealsComponent.css"
 
-export default function Allmeals() {
+export default function Allmeals(props) {
+    const {mealsLink} = props;
+    let mealsLinkPath;
+    const location = useLocation()
+    const fromReviewMeal = location.state?.fromReviewMeal
     const [allMeals, setAllMeals] = useState([]);
     const [isLoading, setIsloading] = useState(false);
 
-    useEffect(() => {
+    const fetchAllMeals = () => {
         setIsloading(true);
         fetch("/api/meals")
           .then((r) => r.json())
@@ -13,26 +20,44 @@ export default function Allmeals() {
           .then(() => {
             setIsloading(false);
           });
-      }, []);
+    }
 
+    useEffect(() => {
+        fetchAllMeals();
+    }, []);
+    
+    if(mealsLink)
+        mealsLinkPath = "/meals/";
+    
     return (
-        <div className="allMealsDiv">
-            <h1>ALL MEALS</h1>
+        <div className="contentDisplayDiv">
+            <h1 style={{fontStyle:"italic"}} className="contentDisplayDivheader">Menu</h1>
             {isLoading && <p>loading....</p>}
             {allMeals.length === 0  ? 
-                    <p>No Results</p> : 
-                    <ul>
+                    <p>No meals are available</p> : 
+                    <ul  className="contentDisplayDivUl">
                         {allMeals.map((meal => {
                             return(
-                                <li key={meal.id}>
+                                <li key={meal.id} className="listContent">
+                                    <h2 style={{color:"olive"}}>Mealid: {meal.id}</h2>
                                     <h2>Title: {meal.title}</h2>
-                                    <p>Description: {meal.description}</p>
-                                    <p>Price; {meal.price}</p>
+                                    <h3>Description: {meal.description}</h3>
+                                    <h4>Max_reservations: {meal.max_reservations}</h4>
+                                    <h4>Price: {meal.price}</h4>
+                                    {mealsLink && !fromReviewMeal && <button><Link to={{pathname: mealsLinkPath+meal.id,
+                                                                state: { fromReserveMeal: true },
+                                                                }} >Reserve Meal</Link></button>}
+                                    {fromReviewMeal && <button><Link to={{pathname: mealsLinkPath+meal.id,
+                                                                state: { fromReviewMeal: true },
+                                                                }} >Review Meal</Link></button>}
                                 </li>
                             )
                         }))}
                     </ul>
             }
+            <div className="topButtonDiv">
+                <button><a href="#header">⇪</a></button>
+            </div>
         </div>
     )
 }
