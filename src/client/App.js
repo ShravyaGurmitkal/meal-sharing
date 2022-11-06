b6051843564f70f7af9e61772acace744d16db8d
shravyagrk@gmail.com <shravyagrk@gmail.com>
2022-11-06 03:57:51 +0100

    client app.js file

--- src/client/App.js
+++ src/client/App.js
@@ -3,7 +3,11 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css"
 import TestComponent from "./components/TestComponent/TestComponent";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 import Allmeals from "./components/Allmeals";
+import CreateMeal from "./components/MealsComponent/CreateMeal";
+import MealsById from "./components/MealsComponent/MealsById";
+import MealReviews from "./components/MealsComponent/MealReviews";
 
 function App() {
   return (
@@ -11,12 +15,33 @@ function App() {
       <Route exact path="/">
         <Header></Header>
         <Allmeals></Allmeals>
-
+        <Footer></Footer>
       </Route>
-      <Route exact path="/meals/{id}">
+    
+      <Route exact path="/meals/:id">
         <Header></Header>
-        <p>lol</p>
+        <MealsById></MealsById>
+        <Footer></Footer>
       </Route>
+
+      <Route exact path="/meals">
+        <Header></Header>
+        <Allmeals mealsLink={true}></Allmeals>
+        <Footer></Footer>
+      </Route>
+
+      <Route exact path="/createmeal">
+        <Header></Header>
+        <CreateMeal></CreateMeal>
+        <Footer></Footer>
+      </Route>
+
+      <Route exact path="/mealReviews">
+        <Header></Header>
+        <MealReviews></MealReviews>
+        <Footer></Footer>
+      </Route>
+
       <Route exact path="/test-component">
         <TestComponent></TestComponent>
       </Route>
